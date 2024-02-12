3f5c768cf34f91c100cc38c910f105dfcfc643e9
Konstantin <tymkovskiy1337@gmail.com>
2024-02-13 02:31:50 +0300

    Added first working graph

--- src/graph1.js
+++ src/graph1.js
@@ -1,43 +1,104 @@
-function getrandom(num, mul) {
-    var value = [];
-    for (i = 0; i <= num; i++) {
-        var rand = Math.random() * mul;
-        value.push(rand);
+/**
+ * position - 3d vector<int>
+ * velocity - 3d vector<float>
+ * charge can be negative
+ * mass - const
+ * @type {{charge: number, mass: number, positions: {x: *[], y: *[], z: *[]}, velocity: number[]}}
+ */
+let particle = {
+    velocity: [1.0, 1.0, 0.0],
+    charge: 1,
+    mass: 1,
+    positions: {
+        x: [0.0],
+        y: [0.0],
+        z: [0.0]
     }
-    return value;
+};
+
+/**
+ * induction - 3d vector<float>
+ * @type number[]
+ */
+let magneticInduction = [1.0, 0.0, 0.0]
+
+
+function vectorProd(vec1, vec2) {
+    return [
+        vec1[1] * vec2[2] - vec1[2] * vec2[1],
+        vec1[2] * vec2[0] - vec1[0] * vec2[2],
+        vec1[0] * vec2[1] - vec1[1] * vec2[0]
+    ];
 }
 
+function vectorProdWithNum6D(vec, num) {
+    return [num * vec[0], num * vec[1], num * vec[2], num * vec[3], num * vec[4], num * vec[5]]
+}
+
+function vectorSum6D(vec1, vec2) {
+    return [vec1[0] + vec2[0], vec1[1] + vec2[1], vec1[2] + vec2[2],
+        vec1[3] + vec2[3], vec1[4] + vec2[4], vec1[5] + vec2[5]]
+}
+
+function func(vec) {
+    let vel_vec = [vec[3], vec[4], vec[5]]
+    let F = vectorProdWithNum6D(vectorProd(vel_vec, magneticInduction), particle.charge);
+
+    particle.positions.x.push(vel_vec[0])
+    particle.positions.y.push(vel_vec[1])
+    particle.positions.z.push(vel_vec[2])
+
+    return [vel_vec[0], vel_vec[1], vel_vec[2],
+        F[0] / particle.mass, F[1] / particle.mass, F[2] / particle.mass];
+}
+
+const dt = 0.05
+N = 1000
+
+function midpoint() {
+    var t = Array.from(Array(N + 1), (_, k) => k * dt);
+    var y = Array(N + 1).fill(0);
+
+    y[0] = [particle.positions.x[0], particle.positions.y[0], particle.positions.z[0],
+        particle.velocity[0], particle.velocity[1], particle.velocity[2]]
+
+    for (let i = 0; i < N; i++) {
+        const k1 = func(y[i]);
+        const k2 = func(vectorSum6D(y[i], vectorProdWithNum6D(k1, dt / 2)));
+
+        y[i + 1] = vectorSum6D(y[i], vectorProdWithNum6D(k2, dt));
+    }
+
+    return y;
+}
+
+y = midpoint()
+
+particle.positions.x = []
+particle.positions.y = []
+particle.positions.z = []
+
+for (let i = 0; i < y.length; i++) {
+    particle.positions.x.push(y[i][0])
+    particle.positions.y.push(y[i][1])
+    particle.positions.z.push(y[i][2])
+}
+
+console.log(particle.positions)
 
 var data = [
     {
-        opacity: 0.4,
         type: 'scatter3d',
-        x: getrandom(50, -75),
-        y: getrandom(50, -75),
-        z: getrandom(50, -75),
+        mode: 'lines',
+        x: particle.positions.x,
+        y: particle.positions.y,
+        z: particle.positions.z,
     },
 ];
 var layout = {
-    scene: {
-        xaxis: {
-            ticktext: ['H20', 'C02', 'O2'],
-            tickvals: [-30, -45, -65, -10]
-        },
-        yaxis: {
-            nticks: 5,
-            tickfont:
-                {
-                    color: 'green',
-                    family: 'Old Standard TT, serif',
-                    size: 14
-                },
-            ticksuffix: '$'
-        },
-        zaxis: {
-            ticks: 'outside',
-            tick0: 0,
-            tickwidth: 4
-        }
-    },
+    autosize: true,
+
 };
+
 Plotly.newPlot('myDiv', data, layout);
+
