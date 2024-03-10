745e8e2fdf495d97529f9d1c6198f709a87443e3
Konstantin <tymkovskiy1337@gmail.com>
2024-03-10 22:20:54 +0300

    Fix: hotfix formula | task 3

--- src/task2/graph.js
+++ src/task2/graph.js
@@ -3,7 +3,7 @@
  * @source1 https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D0%B3%D0%BD%D0%B8%D1%82%D0%BD%D0%B0%D1%8F_%D0%BF%D0%BE%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%BD%D0%B0%D1%8F
  * @type {number}
  */
-const magneticConst = 1.256637 * Math.PI * Math.pow(10, -6)
+const magneticConst = 1.256637 * Math.pow(10, -6)
 
 let graphData = {
     R: 0,
