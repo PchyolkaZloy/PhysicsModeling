ce09ca02fced25ca7c0f8a0b79071c7b2c45e9c8
Konstantin <tymkovskiy1337@gmail.com>
2024-03-12 00:01:04 +0300

    Fix: drawing graphs now in one method | task 4

--- src/task4/graph.js
+++ src/task4/graph.js
@@ -32,160 +32,6 @@ function countDataForGraphs(magneticInduction, area, rotationFrequency, resistan
     return {emfData, amperageData, timeData};
 }
 
-
-function drawTwoGraphsTogether(emfData, amperageData, timeData) {
-    const emfGraphData = {
-        x: timeData,
-        y: emfData,
-        mode: 'lines',
-        type: 'scatter',
-        name: '$E(t)$',
-        hovertemplate: '<b>E(t)</b>: %{y}<extra></extra>'
-    };
-
-    const amperageGraphData = {
-        x: timeData,
-        y: amperageData,
-        mode: 'lines',
-        type: 'scatter',
-        name: '$I(t)$',
-        hovertemplate: '<b>I(t)</b>: %{y}<extra></extra>'
-    };
-
-    const layout = {
-        title: {
-            text: '$\\text{EMF } E(t) \\text{ and Induced current } I(t)$',
-            font: {
-                size: 20
-            }
-        },
-        yaxis: {
-            showspikes: true,
-            title: {
-                text: '$\\text{EMF } E, V;  \\text{ Induced current } \\frac{V}{\\Omega}$',
-                font: {
-                    size: 20
-                }
-            }
-        },
-        xaxis: {
-            showspikes: true,
-            fixedrange: true,
-            title: {
-                text: '$\\text{Time } t, sec$',
-                font: {
-                    size: 20
-                }
-            }
-        },
-
-        margin: {
-            l: 85,
-            r: 25,
-            t: 85
-        },
-
-        showlegend: true,
-        legend: {
-            font: {
-                size: 18,
-            },
-            x: 1,
-            y: 0.5,
-        },
-    };
-
-    const config = {
-        scrollZoom: true,
-        displayModeBar: true,
-        displaylogo: false,
-        responsive: true,
-    };
-
-    Plotly.newPlot('graph', [emfGraphData, amperageGraphData], layout, config);
-}
-
-
-function drawTwoGraphsSeparately(emfData, amperageData, timeData) {
-    const emfGraphData = {
-        x: timeData,
-        y: emfData,
-        mode: 'lines',
-        type: 'scatter',
-        name: '$E(t)$',
-        hovertemplate: '<b>E(t)</b>: %{y}<extra></extra>'
-    };
-
-    const amperageGraphData = {
-        x: timeData,
-        y: amperageData,
-        mode: 'lines',
-        type: 'scatter',
-        xaxis: 'x2',
-        yaxis: 'y2',
-        name: '$I(t)$',
-        hovertemplate: '<b>I(t)</b>: %{y}<extra></extra>'
-    };
-
-    const layout = {
-        grid: {
-            rows: 2,
-            columns: 1,
-            pattern: 'independent',
-        },
-
-        title: {
-            text: '$\\text{EMF } E(t) \\text{ and Induced current } I(t)$'
-        },
-
-        xaxis: {
-            showspikes: true,
-            fixedrange: true,
-            title: '$\\text{Time } t, sec$',
-        },
-        yaxis: {
-            showspikes: true,
-            title: '$\\text{EMF } E, V$'
-        },
-
-        xaxis2: {
-            showspikes: true,
-            fixedrange: true,
-            title: '$\\text{Time } t, sec$',
-        },
-        yaxis2: {
-            showspikes: true,
-            title: '$\\text{Induced current } I, A$'
-        },
-
-        margin: {
-            l: 65,
-            r: 25,
-            t: 85
-        },
-
-        legend: {
-            font: {
-                family: 'Arial, serif',
-                size: 20,
-                color: 'black',
-            },
-            x: 1,
-            xanchor: 'right',
-            y: 0.5,
-        },
-    };
-
-    const config = {
-        scrollZoom: true,
-        displayModeBar: true,
-        displaylogo: false,
-        responsive: true,
-    };
-
-    Plotly.newPlot('graph', [emfGraphData, amperageGraphData], layout, config);
-}
-
 function drawGraphs(graphData) {
     const {
         emfData,
@@ -199,11 +45,130 @@ function drawGraphs(graphData) {
         graphData.stepCount,
         graphData.endTime);
 
+    const emfGraphData = {
+        x: timeData,
+        y: emfData,
+        mode: 'lines',
+        type: 'scatter',
+        name: '$E(t)$',
+        hovertemplate: '<b>E(t)</b>: %{y}<extra></extra>'
+    };
+
+    const amperageGraphData = {
+        x: timeData,
+        y: amperageData,
+        mode: 'lines',
+        type: 'scatter',
+        name: '$I(t)$',
+        hovertemplate: '<b>I(t)</b>: %{y}<extra></extra>'
+    };
+
+    let layout;
+
     if (graphData.isTogether) {
-        drawTwoGraphsTogether(emfData, amperageData, timeData);
+        layout = {
+            title: {
+                text: '$\\text{EMF } E(t) \\text{ and Induced current } I(t)$',
+                font: {
+                    size: 20
+                }
+            },
+            yaxis: {
+                showspikes: true,
+                title: {
+                    text: '$\\text{EMF } E, V;  \\text{ Induced current } \\frac{V}{\\Omega}$',
+                    font: {
+                        size: 20
+                    }
+                }
+            },
+            xaxis: {
+                showspikes: true,
+                fixedrange: true,
+                title: {
+                    text: '$\\text{Time } t, sec$',
+                    font: {
+                        size: 20
+                    }
+                }
+            },
+
+            margin: {
+                l: 85,
+                r: 25,
+                t: 85
+            },
+
+            showlegend: true,
+            legend: {
+                font: {
+                    size: 18,
+                },
+                x: 1,
+                y: 0.5,
+            },
+        };
     } else {
-        drawTwoGraphsSeparately(emfData, amperageData, timeData);
+        amperageGraphData.xaxis = 'x2';
+        amperageGraphData.yaxis = 'y2';
+        layout = {
+            grid: {
+                rows: 2,
+                columns: 1,
+                pattern: 'independent',
+            },
+
+            title: {
+                text: '$\\text{EMF } E(t) \\text{ and Induced current } I(t)$'
+            },
+
+            xaxis: {
+                showspikes: true,
+                fixedrange: true,
+                title: '$\\text{Time } t, sec$',
+            },
+            yaxis: {
+                showspikes: true,
+                title: '$\\text{EMF } E, V$'
+            },
+
+            xaxis2: {
+                showspikes: true,
+                fixedrange: true,
+                title: '$\\text{Time } t, sec$',
+            },
+            yaxis2: {
+                showspikes: true,
+                title: '$\\text{Induced current } I, A$'
+            },
+
+            margin: {
+                l: 65,
+                r: 25,
+                t: 85
+            },
+
+            legend: {
+                font: {
+                    family: 'Arial, serif',
+                    size: 20,
+                    color: 'black',
+                },
+                x: 1,
+                xanchor: 'right',
+                y: 0.5,
+            },
+        };
     }
+
+    const config = {
+        scrollZoom: true,
+        displayModeBar: true,
+        displaylogo: false,
+        responsive: true,
+    };
+
+    Plotly.newPlot('graph', [emfGraphData, amperageGraphData], layout, config);
 }
 
 const defaultValues = {
