2b932b8a5342f0ba70379c813f738b7677d7e7eb
Konstantin <tymkovskiy1337@gmail.com>
2024-05-01 12:33:12 +0300

    Feat: added task9.html

---
+++ src/task9/graph.js
@@ -0,0 +1,20 @@
+/*
+Sources
+https://sv-journal.org/2020-3/11/ru.pdf
+https://en.wikipedia.org/wiki/Double-slit_experiment
+https://www.youtube.com/watch?v=Wbttrnz7ft4
+*/
+
+function countGraphData(){
+
+}
+
+function drawGraph(){
+
+}
+
+function drawDefaultGraph(){
+
+}
+
+drawDefaultGraph()
