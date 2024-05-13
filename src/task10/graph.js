9fb60bb11112bebf37d53d2212f25ef93942bf39
Konstantin <tymkovskiy1337@gmail.com>
2024-05-13 16:31:19 +0300

    Fix: small code fixes | Task 10

--- src/task10/graph.js
+++ src/task10/graph.js
@@ -13,35 +13,50 @@ function fresnelCoeffT(n1, n2) {
     return (4 * n1 * n2) / ((n2 + n1) ** 2);
 }
 
+
 function opticalDifference(lensRadius, radius, betweenRefractiveIndex, waveLength) {
     return 2 * (lensRadius - Math.sqrt(lensRadius ** 2 - radius ** 2)) * betweenRefractiveIndex + waveLength / 2;
 }
 
-function intensive(waveLength, lensRadius, lensRefractiveIndex, betweenRefractiveIndex, plateRefractiveIndex, radius) {
+function intensive(
+    waveLength,
+    lensRadius,
+    lensRefractiveIndex,
+    betweenRefractiveIndex,
+    plateRefractiveIndex,
+    radius,
+    R12, R23, T12, T21) {
+    return R12 + T12 * R23 * T21 + 2 * Math.sqrt(R12 * R23 * T12 * T21) * Math.cos(
+        2 * Math.PI * opticalDifference(lensRadius, radius, betweenRefractiveIndex, waveLength) / waveLength);
+}
+
+function countGraphData(
+    waveLength,
+    lensRadius,
+    lensRefractiveIndex,
+    betweenRefractiveIndex,
+    plateRefractiveIndex,
+    endRadius,
+    stepCount) {
+    const step = endRadius / stepCount;
+    let intensiveData = [];
+    let radiusData = [];
+
     const R12 = fresnelCoeffR(lensRefractiveIndex, betweenRefractiveIndex);
     const R23 = fresnelCoeffR(betweenRefractiveIndex, plateRefractiveIndex);
 
     const T12 = fresnelCoeffT(lensRefractiveIndex, betweenRefractiveIndex);
     const T21 = fresnelCoeffT(betweenRefractiveIndex, lensRefractiveIndex);
 
-    return R12 + T12 * R23 * T21 + 2 * Math.sqrt(R12 * R23 * T12 * T21) * Math.cos(
-        2 * Math.PI * opticalDifference(lensRadius, radius, betweenRefractiveIndex, waveLength) / waveLength);
-}
-
-function countGraphData(waveLength,
-                        lensRadius,
-                        lensRefractiveIndex,
-                        betweenRefractiveIndex,
-                        plateRefractiveIndex,
-                        endRadius,
-                        stepCount) {
-    const step = endRadius / stepCount;
-    let intensiveData = [];
-    let radiusData = [];
-
     for (let radius = 0; radius <= endRadius; radius += step) {
         intensiveData.push(intensive(
-            waveLength, lensRadius, lensRefractiveIndex, betweenRefractiveIndex, plateRefractiveIndex, radius));
+            waveLength,
+            lensRadius,
+            lensRefractiveIndex,
+            betweenRefractiveIndex,
+            plateRefractiveIndex,
+            radius,
+            R12, R23, T12, T21));
         radiusData.push(radius);
     }
 
