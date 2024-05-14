00793f11ff3d4274b7853e3c33a27e53d94ae3b9
Konstantin <tymkovskiy1337@gmail.com>
2024-05-15 00:12:29 +0300

    Fix: hotfix | Task 10

--- src/task10/graph.js
+++ src/task10/graph.js
@@ -1,8 +1,5 @@
 /*
-Sources
-https://study.physics.itmo.ru/pluginfile.php/4244/mod_resource/content/14/%D0%9E%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5%20%D0%BB%D0%B0%D0%B1%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BD%D0%BE%D0%B9%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B%204.03.pdf
-https://en.wikipedia.org/wiki/Fresnel_equations
-https://pchz.notion.site/2-9b8f874a4c784666802b4790137f81fb?pvs=4
+Sources in .pdf
 */
 
 
